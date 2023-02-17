76006373465b43876098e4a42cc1c011bedb8829
CoderApps <isappiah712@gmail.com>
2023-02-17 08:31:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - a program that print a line with puts function
  *
- * return: 0(Success)
+ * return 0:0(Success)
  *
-  **/
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return (0);/*returns 0*/
 }
