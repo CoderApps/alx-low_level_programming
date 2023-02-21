8d4ac91fefb20d63aff83effe1d5430ca16d5f47
CoderApps <isappiah712@gmail.com>
2023-02-21 11:08:45 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *
  * return 0: #(Success)
  *
- */
-int main(void)
+  **/
+int main(void)/* main function */
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);/*returns 0*/
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,9 @@
-#include <s
+#include <stdio.h>
+/*
+ * main - A program that print a line using printf
+ * return:(0) #(success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+	return (0)
